35eea9fc7e680e774a9bdbefe99b359f9415face
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2025-10-06 08:08:22 -0500

    Creación del nuevo microservicio service-offering con su modelo ServiceOffering y estructura base del proyecto

---
+++ service-offering/src/main/java/com/oldtownbarber/service_offering/model/ServiceOffering.java
@@ -0,0 +1,42 @@
+package com.oldtownbarber.service_offering.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "services")
+public class ServiceOffering {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private Long salonId;
+
+    @Column(nullable = false)
+    private Long categoryId;
+
+    private String image;
+
+
+
+
+}
