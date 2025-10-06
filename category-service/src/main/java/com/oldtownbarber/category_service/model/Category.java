8a96fb9a06d730baba4af696c76f2deda5fc069d
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2025-10-05 23:01:28 -0500

    Se creo el modelo Category y el archivo application.properties

---
+++ category-service/src/main/java/com/oldtownbarber/category_service/model/Category.java
@@ -0,0 +1,30 @@
+package com.oldtownbarber.category_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categorys")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String image;
+
+    @Column(nullable = false)
+    private Long salonId;
+
+
+
+
+}
